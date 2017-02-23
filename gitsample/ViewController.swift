14b39f2affc0e8a4c4eafd1377787ccc5eca6816
vishnumls <vishnu@mind-labs.com>
2017-02-23 14:45:19 +0530

    Initial Commit

---
+++ gitsample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitsample
+//
+//  Created by vishnu Mindbees on 23/02/17.
+//  Copyright © 2017 Mindlabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
